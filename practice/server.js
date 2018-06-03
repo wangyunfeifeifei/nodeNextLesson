8d4b7637d365134ae1f572bc4863d82ab4872394
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-06-03 10:50:10 +0800

    feat: 增加server.js

---
+++ practice/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const HOST = '127.0.0.1'
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+})
+
+server.listen(PORT, HOST, () => {
+    console.log(`server is start at port ${3000}`);
+})
