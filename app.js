4507b426d35d006734c63cd6d428d224b8b88fe9
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-06-03 16:04:50 +0800

    增加app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const querystring = require('querystring');
+const url = require('url');
+
+const Config = {
+    host: '127.0.0.1',
+    port: 3000
+}
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    let o = url.parse(req.url).query || '';
+    res.end(JSON.stringify(querystring.parse(o)));
+});
+
+server.listen(Config.port,Config.host, () => {
+    console.log(`server is start at http://${Config.host}:${Config.port}`)
+});
